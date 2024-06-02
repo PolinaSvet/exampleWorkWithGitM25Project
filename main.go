3b86ca77277bef0ebdde87aac091f710335d0c10
Polina Svet <polinaSvet6@gmail.com>
2024-06-02 12:56:06 +0400

    Commit 3: Fix mistakes echo server could accept arbitrary data, not just integers

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 )
 func main() {
     n := ""
-    fmt.Print("Enter an integer: ")
+    fmt.Print("Enter data: ")
     _, err := fmt.Scan(&n)
     if err != nil {
         log.Fatal(err)
     }
-    fmt.Printf("You have entered a number: %s\n", n)
+    fmt.Printf("You have entered the following information: %s\n", n)
 }
